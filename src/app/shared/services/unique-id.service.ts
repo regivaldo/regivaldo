ba1c300bd588328e039ab5f875741fb28dcc9a2e
Regivaldo Silva <regivaldorfs@gmail.com>
2022-03-02 16:35:32 -0300

    ajustes

---
+++ src/app/shared/services/unique-id.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UniqueIdService {
+
+  private numberOfGeneratedIds = 0;
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
+
+  constructor() { }
+
+  generateUniqueIdWithPrefix(prefix: string): string {
+    if (!prefix || !this.validId.test(prefix)) {
+      throw Error('Prefix can not be empty');
+    }
+
+    const uniqueId = this.generateUniqueId();
+    this.numberOfGeneratedIds++;
+    return `${prefix}-${uniqueId}`;
+  }
+
+  public getNumberOfGeneratedUniqueIds(): number {
+    return this.numberOfGeneratedIds;
+  }
+
+  private generateUniqueId(): string {
+    return uuidv4();
+  }
+}
