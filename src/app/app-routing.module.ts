0310b783048572f16b21b6b86b2095f99527e54b
Regivaldo Silva <regivaldorfs@gmail.com>
2022-11-14 16:31:39 -0300

    Ajuste na rota principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/landing',
-    pathMatch: 'full'
-  },
-  {
-    path: 'landing',
     loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule)
   }
 ];
